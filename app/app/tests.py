30011e532d457dc5fe3b77086e17bbc2da752b82
Jan Elbert M. David <j.david.496751@umindanao.edu.ph>
2024-02-26 13:09:12 +0800

    Added PostGres DB.

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+# Sample tests
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    #Test calc module
+    
+    def test_add_numbers(self):
+        #Test add numbers together
+        res = calc.add(5, 6)
+        
+        self.assertEquals(res, 11)
+    
+    def test_subtract_numbers(self):
+        #Subtract numbers
+        
+        res=calc.subtract(10, 15)
+        
+        self.assertEquals(res, 5)

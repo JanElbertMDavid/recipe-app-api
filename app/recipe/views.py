9e3e8300d8e24b5903d3a0038cc027f6ec6b4470
Jan Elbert M. David <j.david.496751@umindanao.edu.ph>
2024-03-02 23:26:05 +0800

    Step 15.2 done

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -37,15 +37,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin,
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
-    '''Manage tags in the database.'''
+    """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        '''Filter queryset to authenticated user.'''
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
